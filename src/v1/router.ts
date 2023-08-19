87f0a7611e2600bd838e70c86784092356f8beeb
Nick Wong <njwon4@gmail.com>
2023-08-19 11:56:40 +1000

    feat: add link to docs in v1 404 page

--- src/v1/router.ts
+++ src/v1/router.ts
@@ -6,4 +6,22 @@ const router = Router({ base: '/api/v1' });
 
 router.get('/characters/*', charactersRouter.handle);
 
+// Show page that links to the docs
+router.all('*', () => {
+	const html = `
+		<html>
+			<head>
+				<title>ME-API - v1</title>
+			</head>
+			<body>
+				<h1>Not Found</h1>
+				<p>The requested resource could not be found.</p>
+				<p>You can find the documentation for the API at <a href="https://me-api-docs.pages.dev">https://me-api-docs.pages.dev</a></p>
+			</body>
+		</html>
+	`;
+
+	return new Response(html, { status: 404, headers: { 'Content-Type': 'text/html' } });
+});
+
 export default router;
