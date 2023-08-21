2da86d2c8e1f42a8b64003582a5cda06f64d8c09
Nick Wong <njwon4@gmail.com>
2023-08-21 11:18:15 +1000

    fix: for base path responses, return some HTML with links to get to the docs

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,27 @@
 import { Router } from 'itty-router';
 import routerV1 from './v1/router';
 
-const router = Router({ base: '/api' });
+const router = Router();
 
 // v1 API handler
-router.get('/v1/*', routerV1.handle);
+router.get('/api/v1/*', routerV1.handle);
 
 // 404 for everything else
-router.all('*', () => new Response('Not Found.', { status: 404 }));
+router.all('*', () => {
+	const html = `
+		<html>
+			<head>
+				<title>ME-API</title>
+			</head>
+			<body>
+				<h1>Welcome!</h1>
+				<p>This is the base path for ME-API, so there's nothing here. The v1 API starts <a href="/api/v1">here!</a></p>
+				<p>You can find the documentation for the API at <a href="https://me-api-docs.pages.dev">https://me-api-docs.pages.dev</a></p>
+			</body>
+		</html>
+	`;
+
+	return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html' } });
+});
 
 export default router;
--- src/v1/router.ts
+++ src/v1/router.ts
@@ -15,17 +15,17 @@ router.all('*', () => {
 	const html = `
 		<html>
 			<head>
-				<title>ME-API - v1</title>
+				<title>ME-API - v1 API</title>
 			</head>
 			<body>
-				<h1>Not Found</h1>
-				<p>The requested resource could not be found.</p>
-				<p>You can find the documentation for the API at <a href="https://me-api-docs.pages.dev">https://me-api-docs.pages.dev</a></p>
+				<h1>ME-API - v1 API</h1>
+				<p>Welcome! You've found the base path for the v1 API, so there's nothing here.</p>
+				<p>Go to a path like <a href="/api/v1/characters">/api/v1/characters</a> to see some data, or check out the API documentation website at <a href="https://me-api-docs.pages.dev">https://me-api-docs.pages.dev</a>.</p>
 			</body>
 		</html>
 	`;
 
-	return new Response(html, { status: 404, headers: { 'Content-Type': 'text/html' } });
+	return new Response(html, { status: 200, headers: { 'Content-Type': 'text/html' } });
 });
 
 export default router;
